592400d4bd03e082de4b7f2e2ad2218aa6b1070e
nahidularafat <146655188+nahidularafat@users.noreply.github.com>
2024-08-14 12:58:09 +0600

    Create 13.StringsTL.cpp

---
+++ 13.StringsTL.cpp
@@ -0,0 +1,27 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+string name ;
+//cin>>name ; // word;
+//getline(cin,name); // sectence 
+//name = name + " arafat";
+//name.push_back('y');
+//cout<<name.length()<<endl;
+//cout<<name<<endl;
+   
+    
+    string x = "arafat";
+    string y= "nahidul arafat";
+    if(x.compare(y)==0)
+    {
+        cout<<"same";
+    }
+    else
+    {
+        cout<<"different"<<endl;
+    }
+    
+    cout<<y.find("arafat");
+}
